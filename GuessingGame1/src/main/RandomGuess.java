b20a7c7b65c8e1755f2c6c5d17897d3fa53d9167
csmahoney <csmahoney627@gmail.com>
2023-11-14 23:56:41 -0500

    Update RandomGuess.java

--- GuessingGame1/src/main/RandomGuess.java
+++ GuessingGame1/src/main/RandomGuess.java
@@ -4,67 +4,55 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class RandomGuess {
-
 	public static void main(String[] args) {
 
-		
-
 		Scanner sc = new Scanner(System.in);
-
 		Random rand = new Random();
+		
 
-		
-		
 		int upperbound = 10;
 		int random_number = rand.nextInt(upperbound);
-
 		boolean gameComplete = false;
-
-		int numGuesses = 0;
-
+		int numGuesses = 5;
 		
-		System.out.println("Please guess a random number 1-10, if your guess is wrong please try again.");
 
+		System.out.println("Please guess a random number 1-10, if your guess is wrong, please try again.");
 		System.out.println("You only have 5 guesses to get it correct!");
-				
-			while (gameComplete == false) {
+		
+
+			while (gameComplete == false) { //Runs a loop so the user can continue to guess
+
 				int number_guess = sc.nextInt();
 
-				
-
-				if (number_guess == random_number) {
-
-				System.out.println("Congradulations, you guessed the correct number!");
-
-				gameComplete = true;
-
+				if (number_guess == random_number) { // Ends the game if the user correctly guesses the random number
+					System.out.println("Congradulations, you guessed the correct number!");
+					gameComplete = true;
 				}
 
-				else if (number_guess != random_number && number_guess >= 1 && number_guess <= 9) {
-
-					numGuesses++;
-
-					if (numGuesses != 5) {
-
-						System.out.println("This is not the correct number, guess again!");
-
+				else if (number_guess != random_number && number_guess >= 1 && number_guess <= 10) { // Defines the bounds the user can guess, must be between 0 and 10.
+					numGuesses--;
+					if (numGuesses != 0) {
+						System.out.println("This is not the correct number, guess again! You have " + numGuesses + " guesses remaining.");
 					}
 
-					else if (numGuesses == 5) {
-
+					else if (numGuesses == 0) {
 						System.out.println("You have exceeded your maximum number of guesses without a correct answer, sucks to suck!");
-
 						System.exit(-1);
-
 					}
+
 				}
-				else if (number_guess >= 11 && number_guess <= 0) {
-					System.out.println("That number is not within the bounds, please pick a number between 1 and 10.");
+
+				else {
+					System.out.println("This number is not between 1 and 10, please guess again. You have " + numGuesses + "guesses remaining.");
+					numGuesses--;
 				}
-				
-						
+	
+
+			}
 
 		}
+	
+	
+
 
 	}
-}
